05c3d501a5de2ae3ea044b5768f86c40e784dab6
cometadraco <128496049+cometadraco@users.noreply.github.com>
2025-03-28 15:43:31 -0500

    fix(gHappy): corregido el índice fuera de rango test(GHappyTest): agregadas pruebas unitarias para validar la funcionalidad

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,40 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+
 public class GHappyTest {
+
+    private final GHappy gHappy = new GHappy();
+
+    @Test
+    public void testGHappy1() {
+        assertTrue(gHappy.gHappy("xxggxx"));
+    }
+
+    @Test
+    public void testGHappy2() {
+        assertFalse(gHappy.gHappy("xxgxx"));
+    }
+
+    @Test
+    public void testGHappy3() {
+        assertFalse(gHappy.gHappy("xxggyygxx"));
+    }
+
+    @Test
+    public void testGHappy4() {
+        assertTrue(gHappy.gHappy("gggg"));
+    }
+
+    @Test
+    public void testGHappy5() {
+        assertTrue(gHappy.gHappy("g"));
+    }
+
+    @Test
+    public void testGHappy6() {
+        assertTrue(gHappy.gHappy(""));
+    }
+
 }
